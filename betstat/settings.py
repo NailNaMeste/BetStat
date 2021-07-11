594699e4b97f9e173ed79d48d5828d52fef2096a
Nail Irtuganov <74359307+NailNaMeste@users.noreply.github.com>
2021-07-11 16:06:31 +0300

    Update settings.py

--- betstat/settings.py
+++ betstat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+tw-uo-0#i=dgt^1kw54be%#ewdtreix9*4a0m2s3r)sk&zo-n'
+SECRET_KEY = [secret_key228]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
